072e0d5cc85720de455f877b3e24ae611840e1fa
unknown <hujj@HUJJ.sitco.com>
2016-08-29 09:48:27 +0800

    更新： 1.people页面跳转到display 2.user页面功能实现 3.userdoc页面功能实现 4.config链接 5.登录界面增加用户id

--- pagespc/js/config.js
+++ pagespc/js/config.js
@@ -3,7 +3,9 @@
         if (actionName == null || actionName.trim() == '' || actionName.toLowerCase() == 'undefined') {
             actionName = '';
         }
-        return "http://5776920e23f5a.sh.cdb.myqcloud.com:4770/QiJian/" + actionName;
+        //return "http://5776920e23f5a.sh.cdb.myqcloud.com:4770/QiJian/" + actionName;
+        return "http://localhost:8080//QiJian/" + actionName;
+//        return "http://115.159.217.201:7001//QiJian/" + actionName;
     }
 
 });
